605098a366a01a459d76ea624a2efc8196862867
unknown <b00145533@PC-P-E219-02.student.uws.loc>
2015-11-04 13:03:07 +0000

    this was B00145533

--- hello.cpp
+++ hello.cpp
@@ -5,9 +5,10 @@ int main(int argc, char *argv[])
 {
 	//bitches be like O.O
   std::cout << "Hello World.\n";
-  std::cout << "Do you know that a tall ginger man was here?!\n"; 
+  std::cout << "Do you know that a tall ginger man was here?!\n"; //unfortunately for humanity
   std::cout << "Hello again" << endl; 
   std::cout << "Stu was here too" <<endl; //this was Stuart Cullen B00136830
+  std::cout << "namespace4life!" <<endl;//b00145533
   return 0;
 }
 
