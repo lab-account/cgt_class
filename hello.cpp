27334c3c6b3b11f25e40039cc4d6708c9b71b696
Stuart Cullen <stu2408.sc@gmail.com>
2015-11-04 12:45:32 +0000

    This was B00136830

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,7 @@ int main(int argc, char *argv[])
 {
   std::cout << "Hello World.\n";
   std::cout << "Hello again" << endl; 
+  std::cout << "Stu was here too" <<endl; //this was Stuart Cullen B00136830
   return 0;
 }
 
