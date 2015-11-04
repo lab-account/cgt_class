69f62b2958d6420421902fa042e84d3994ad7945
unknown <B00252361@PC-P-E219-10.student.uws.loc>
2015-11-04 12:38:27 +0000

    update

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+//Somewords
 
 int main(int argc, char *argv[])
 {
   std::cout << "Hello World.\n";
+  std::cout << "Do you know that a tall ginger man was here?!\n"; 
   std::cout << "Hello again" << endl; 
   return 0;
 }
