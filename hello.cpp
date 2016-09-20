43ab65d6cd52172d384cf45f02d02eccbc57cf7d
pkeir <paul.keir@uws.ac.uk>
2016-09-20 22:00:42 +0100

    Reset.

--- hello.cpp
+++ hello.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
-//Somewords
 
 int main(int argc, char *argv[])
 {
-	//bitches be like O.O
   std::cout << "Hello World.\n";
-  std::cout << "Do you know that a tall ginger man was here?!\n"; //unfortunately for humanity
-  std::cout << "Hello again" << std::endl; 
-  std::cout << "Stu was here too" <<std::endl; //this was Stuart Cullen B00136830
-  std::cout << "namespace4life!" <<std::endl;//b00145533
   return 0;
 }
 
